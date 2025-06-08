07aef662d960165d26bd91c69f24eae9e1b10ce5
BrakhmaQA <mmarol9233@gmail.com>
2025-06-08 23:11:45 +0300

    Изменил логику

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,50 +1,102 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.calculator.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.ExpressionBuilder
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
-    private val actions: Actions = Actions()
+    private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(binding?.root)
 
-        binding.btn0.setOnClickListener { actions.setTextFields(binding, "0") }
-        binding.btn1.setOnClickListener { actions.setTextFields(binding, "1") }
-        binding.btn2.setOnClickListener { actions.setTextFields(binding, "2") }
-        binding.btn3.setOnClickListener { actions.setTextFields(binding, "3") }
-        binding.btn4.setOnClickListener { actions.setTextFields(binding, "4") }
-        binding.btn5.setOnClickListener { actions.setTextFields(binding, "5") }
-        binding.btn6.setOnClickListener { actions.setTextFields(binding, "6") }
-        binding.btn7.setOnClickListener { actions.setTextFields(binding, "7") }
-        binding.btn8.setOnClickListener { actions.setTextFields(binding, "8") }
-        binding.btn9.setOnClickListener { actions.setTextFields(binding, "9") }
-        binding.btnSubtract.setOnClickListener { actions.setTextFields(binding, "-") }
-        binding.btnSum.setOnClickListener { actions.setTextFields(binding, "+") }
-        binding.btnMultiply.setOnClickListener { actions.setTextFields(binding, "*") }
-        binding.btnDivide.setOnClickListener { actions.setTextFields(binding, "/") }
-        binding.btnBracketOpen.setOnClickListener { actions.setTextFields(binding, "(") }
-        binding.btnBracketClose.setOnClickListener { actions.setTextFields(binding, ")") }
-        binding.btnDot.setOnClickListener { actions.setTextFields(binding, ".") }
-
-        binding.btnClear.setOnClickListener {
-            actions.clearMathOperation(binding)
-            actions.clearOperationResult(binding)
+        for (button in getButtonList()) {
+            button.key.setOnClickListener { setTextFields(button.value) }
         }
 
-        binding.btnBack.setOnClickListener {
-            actions.deleteDigitsInMathOperationField(binding)
-            actions.clearOperationResult(binding)
+        binding?.apply {
+            btnClear.setOnClickListener {
+                mathOperation.clearText()
+                operationResult.clearText()
+            }
+
+            btnBack.setOnClickListener {
+                mathOperation.text = mathOperation.text.dropLast(1)
+                operationResult.clearText()
+            }
+
+            btnEqual.setOnClickListener {
+                getEvaluationResult()
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+
+    private fun getButtonList(): MutableMap<TextView, String> {
+        val buttons = mutableMapOf<TextView, String>()
+
+        binding?.apply {
+            buttons += btn0 to "0"
+            buttons += btn1 to "1"
+            buttons += btn2 to "2"
+            buttons += btn3 to "3"
+            buttons += btn4 to "4"
+            buttons += btn5 to "5"
+            buttons += btn6 to "6"
+            buttons += btn7 to "7"
+            buttons += btn8 to "8"
+            buttons += btn9 to "9"
+            buttons += btnSubtract to "-"
+            buttons += btnSum to "+"
+            buttons += btnMultiply to "*"
+            buttons += btnDivide to "/"
+            buttons += btnBracketOpen to "("
+            buttons += btnBracketClose to ")"
+            buttons += btnDot to "."
         }
 
-        binding.btnEqual.setOnClickListener {
-            actions.getEvaluationResult(binding)
+        return buttons
+    }
+
+    private fun TextView.clearText() {
+        this.text = ""
+    }
+
+    private fun getEvaluationResult() {
+        binding?.apply {
+            try {
+                val ex = ExpressionBuilder(mathOperation.text.toString()).build()
+                val result = ex.evaluate().toBigDecimal().stripTrailingZeros()
+                round(result.toDouble())
+
+                operationResult.text = result.toString()
+            } catch (e: ArithmeticException) {
+                Log.d("Ошибка", "сообщение: ${e.message}")
+            } catch (e: Exception) {
+                Log.d("Ошибка", "сообщение: ${e.message}")
+            }
+        }
+    }
+
+    private fun setTextFields(str: String) {
+        binding?.apply {
+            if (operationResult.text != "") {
+                mathOperation.text = operationResult.text
+                operationResult.clearText()
+            }
+
+            mathOperation.append(str)
         }
     }
 }
